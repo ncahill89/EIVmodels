a17e6501d12dcc0b6d5a317a6d8510a8fc78a634
Niamh Cahill <niamh.cahill@mu.ie>
2022-04-11 22:59:27 +0100

    add data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' A sea level reconstruction from New Jersey, USA.
+#'
+#' A dataset containing the relative sea level (RSL) reconstruction, the RSL error, the age reconstruction and the agg error
+#'
+#' @format A data frame with 135 rows and 4 variables:
+#' \describe{
+#'   \item{x}{age, in year CE}
+#'   \item{y}{relative sea level in metres}
+#'   \item{x_err}{age error}
+#'   \item{y_err}{RSL error}
+#' }
+#' @source \url{https://cp.copernicus.org/articles/12/525/2016/}
+"NJ_CC"
